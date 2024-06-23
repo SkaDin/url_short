99a1ffae1fc111407b7672ac37379f17faa300ed
SkaDin <ga.akam@yandex.ru>
2024-06-23 19:00:32 +0300

    prettyLogger

--- cmd/url_short/main.go
+++ cmd/url_short/main.go
@@ -6,6 +6,8 @@ import (
 	"log/slog"
 	"os"
 	"url_short/internal/config"
+	mv "url_short/internal/http-server/middleware/logger"
+	"url_short/internal/lib/logger/handlers/slogpretty"
 	"url_short/internal/lib/logger/sl"
 	"url_short/internal/storage/sqlite"
 )
@@ -23,18 +25,24 @@ func main() {
 
 	log.Info("starting url_short", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+	log.Error("error")
+	log.Warn("warn")
 
 	storage, err := sqlite.New(cfg.StoragePath)
+	_ = storage
 	if err != nil {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
-	_ = storage
+
 	router := chi.NewRouter()
 
 	router.Use(middleware.RequestID)
 	router.Use(middleware.RealIP)
 	//router.Use(middleware.Logger)
+	router.Use(mv.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 }
 
 func setupLogger(env string) *slog.Logger {
@@ -42,9 +50,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -55,4 +61,14 @@ func setupLogger(env string) *slog.Logger {
 		)
 	}
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOption{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+	handler := opts.NewPrettyHandler(os.Stdout)
+	return slog.New(handler)
 }
